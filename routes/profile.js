2e7e4fc3f9c9fb8e48eaaba594a62c94ad8543a9
Francesca Ropolo <franci.ropolo@gmail.com>
2018-09-24 17:47:26 +0200

    owner in bold

--- routes/profile.js
+++ routes/profile.js
@@ -20,4 +20,19 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
+router.get('/edit', (req, res, next) => {
+  const user = req.session.currentUser
+  console.log(user)
+  if (!req.session.currentUser) {
+    return res.redirect('/auth/login')
+  }
+  User.findById(user._id)
+    .then((result) => {
+      const data = { user: result }
+
+      res.render('layout-profile', data)
+    })
+    .catch(next)
+})
+
 module.exports = router
