674b270ddac508489d8e36123e601253a6d87544
Francesca Ropolo <franci.ropolo@gmail.com>
2018-09-24 18:49:13 +0200

    edit profile page and update data

--- routes/profile.js
+++ routes/profile.js
@@ -22,7 +22,6 @@ router.get('/', (req, res, next) => {
 
 router.get('/edit', (req, res, next) => {
   const user = req.session.currentUser
-  console.log(user)
   if (!req.session.currentUser) {
     return res.redirect('/auth/login')
   }
@@ -30,7 +29,28 @@ router.get('/edit', (req, res, next) => {
     .then((result) => {
       const data = { user: result }
 
-      res.render('layout-profile', data)
+      res.render('profile-edit', data)
+    })
+    .catch(next)
+})
+
+router.post('/edit', (req, res, next) => {
+  const user = req.session.currentUser
+  if (!req.session.currentUser) {
+    return res.redirect('/auth/login')
+  }
+  console.log(req.body)
+  const { username, description } = req.body
+  if (!username || !description) {
+    req.flash('yourney-form-error', 'Please fill the fields')
+    req.flash('yourney-form-data', { username, description })
+    return res.redirect('/edit')
+  }
+  const update = { username, description }
+  User.findByIdAndUpdate(user._id, update, { new: true })
+    .then((result) => {
+      req.session.currentUser = result
+      res.redirect('/')
     })
     .catch(next)
 })
