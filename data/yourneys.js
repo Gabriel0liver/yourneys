cd5187052cd1aced6053e3d48cc3fb583c31a74a
MJHR <majohr89@gmail.com>
2018-09-28 14:57:27 +0200

    clean seed

--- data/yourneys.js
+++ data/yourneys.js
@@ -1,27 +1,3 @@
-const data = [{
-  name: 'BCN for dogs',
-  snippet: 'Wanna enjoy Barcelona with your best friend?',
-  description: 'Mister Guau Center, located at L’Eixample, (Carrer d’Aribau, 21), Animals & Cia, located at L’Eixample, (Gran Via de les Corts Catalanes, 513), Barkcelona, located at L’Eixample, (Carrer d’Aribau, 43)',
-  location: 'Barcelona',
-  date: '2018/09/22',
-  days: '1'
-},
-{
-  name: 'BCN Night life',
-  snippet: 'Discover the best parties for dogs',
-  description: 'Mister Guau Center, located at L’Eixample, (Carrer d’Aribau, 21), Animals & Cia, located at L’Eixample, (Gran Via de les Corts Catalanes, 513), Barkcelona, located at L’Eixample, (Carrer d’Aribau, 43)',
-  location: 'Barcelona',
-  date: '2018/09/22',
-  days: '1'
-},
-{
-  name: 'In da Park',
-  snippet: 'Best parks to go for a walk with your hooman',
-  description: 'Mister Guau Center, located at L’Eixample, (Carrer d’Aribau, 21), Animals & Cia, located at L’Eixample, (Gran Via de les Corts Catalanes, 513), Barkcelona, located at L’Eixample, (Carrer d’Aribau, 43)',
-  location: 'London',
-  date: '2018/09/22',
-  days: '1'
-}
-];
+const data = [];
 
 module.exports = data;
